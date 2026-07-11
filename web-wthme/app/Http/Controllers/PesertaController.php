97d9b4433cd9da72d0fef629694b03151cca0bfb
DX4GREY <dxablack@gmail.com>
2026-07-12 03:42:09 +0700

    Add personal broadcasts retrieval in PesertaController with error handling

--- web-wthme/app/Http/Controllers/PesertaController.php
+++ web-wthme/app/Http/Controllers/PesertaController.php
@@ -130,6 +130,30 @@ class PesertaController extends Controller
             }
         }
 
+        // Personal broadcasts (safely load if tables exist)
+        $personalBroadcasts = collect();
+        try {
+            if (Schema::hasTable('personal_broadcasts') && Schema::hasTable('personal_broadcast_recipients')) {
+                $personalBroadcasts = PersonalBroadcastRecipient::where('user_id', $user->id)
+                    ->with('broadcast')
+                    ->latest()
+                    ->get()
+                    ->groupBy('personal_broadcast_id')
+                    ->filter(function ($group) {
+                        return !$group->contains(function ($recipient) {
+                            return !is_null($recipient->viewed_at);
+                        });
+                    })
+                    ->map(function ($group) {
+                        return $group->first();
+                    })
+                    ->values();
+            }
+        } catch (\Throwable $e) {
+            report($e);
+            $personalBroadcasts = collect();
+        }
+
         return view('peserta.index', compact(
             'user', 
             'sudahIsiRiwayat', 
