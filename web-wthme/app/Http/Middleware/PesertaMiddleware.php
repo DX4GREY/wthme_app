7bfb04efeff7e3afef24c3ff175dd533113f7ebd
DX4GREY <dxablack@gmail.com>
2026-07-13 15:11:40 +0700

    Fix: Update isPeserta method to include 'admin' role and adjust access error message in PesertaMiddleware

--- web-wthme/app/Http/Middleware/PesertaMiddleware.php
+++ web-wthme/app/Http/Middleware/PesertaMiddleware.php
@@ -12,7 +12,7 @@ class PesertaMiddleware
     {
         if (!auth()->check() || !auth()->user()->isPeserta()) {
             return redirect()->route('dashboard')
-                ->with('error', 'Akses ditolak. Halaman ini untuk peserta atau admin.');
+                ->with('error', 'Akses ditolak. Halaman ini untuk peserta.');
         }
         return $next($request);
     }
